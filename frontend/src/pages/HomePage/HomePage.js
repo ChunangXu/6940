f556bb25494e374558845781176e4492cf5e981b
ChunangXu <70995317+ChunangXu@users.noreply.github.com>
2025-05-23 18:38:13 -0400

    Add basic router and homepage for frontend

---
+++ frontend/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './HomePage.css';
+
+export default function HomePage() {
+  return (
+    <div className="home-container">
+      <h1>Welcome</h1>
+    </div>
+  );
+}
